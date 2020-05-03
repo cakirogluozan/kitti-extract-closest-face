62f6c301c0eebec48e3c91a9b089ea38fc057108
Ozan Cakiroglu <cakirogluozan@gmail.com>
2020-05-03 11:17:22 +0300

    bugfix

--- src/write_textfile.py
+++ src/write_textfile.py
@@ -5,9 +5,17 @@ from utils import write_labels
 if __name__ == '__main__':
 
     # TODO:
+    dgxs=True
+
     CALIB_DIR = "/raid/data/l4data/benchmark_data/kitti-ozan/object/data_object_calib/training/calib"
     IMAGE_DIR = "/raid/data/l4data/benchmark_data/kitti-ozan/object/data_object_image_2/training/image_2"
     LABEL_DIR = "/raid/data/l4data/benchmark_data/kitti-ozan/object/training/label_2"
+    if dgxs:
+        CALIB_DIR = CALIB_DIR.replace('/raid', '/mnt/dgx1_data')
+        IMAGE_DIR = IMAGE_DIR.replace('/raid', '/mnt/dgx1_data')
+        LABEL_DIR = LABEL_DIR.replace('/raid', '/mnt/dgx1_data')
+    ######
+
 
     calib_list = [os.path.join(CALIB_DIR, png) for png in os.listdir(CALIB_DIR) if png.endswith('.txt')]
     calib_list.sort()
